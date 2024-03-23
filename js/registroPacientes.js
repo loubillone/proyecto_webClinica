33d01407589117aa3ba0bcd6d20cb8ccf3794749
Lourdes Billone <lou.billone@gmail.com>
2024-03-23 15:42:28 -0300

    agrego sweetAlert a registro pacientes JS, modifico descripción de form de registro médico en html

--- js/registroPacientes.js
+++ js/registroPacientes.js
@@ -1,83 +1,93 @@
-let usuariosPaciente = JSON.parse(localStorage.getItem("usuariosPaciente")) || [];
+let usuariosPaciente =
+  JSON.parse(localStorage.getItem("usuariosPaciente")) || [];
 let formulario = document.getElementById("formulario");
 
 class Paciente {
-    constructor(
-        nombreCompleto,
-        fechaNac,
-        email,
-        contraseña,
-        genero,
-        obraSocial,
-        rol = "paciente"
-    ) {
-        this.nombreCompleto = nombreCompleto;
-        this.fechaNac = fechaNac;
-        this.email = email;
-        this.contraseña = contraseña;
-        this.genero = genero,
-        this.obraSocial = obraSocial,
-        this.rol = rol;
-    }
+  constructor(
+    nombreCompleto,
+    fechaNac,
+    email,
+    contraseña,
+    genero,
+    obraSocial,
+    rol = "paciente"
+  ) {
+    this.nombreCompleto = nombreCompleto;
+    this.fechaNac = fechaNac;
+    this.email = email;
+    this.contraseña = contraseña;
+    (this.genero = genero), (this.obraSocial = obraSocial), (this.rol = rol);
+  }
 }
 
 const handleSubmit = (e) => {
-    e.preventDefault();
-  
-    validarPaciente();
-  };
+  e.preventDefault();
 
-const validarPaciente = (e) => {
+  validarPaciente();
+};
 
+const validarPaciente = () => {
+  let nombreCompleto = document.getElementById("nombreCompleto").value;
+  let fechaNac = document.getElementById("fechNac").value;
+  let email = document.getElementById("email").value;
+  let contraseña = document.getElementById("contraseña").value;
+  let genero = document.getElementById("genero").value;
+  let obraSocial = document.getElementById("obraSocial").value;
+  let confPass = document.getElementById("confPass").value;
 
-    let nombreCompleto = document.getElementById("nombreCompleto").value;
-    let fechaNac = document.getElementById("fechNac").value;
-    let email = document.getElementById("email").value;
-    let contraseña = document.getElementById("contraseña").value;
-    let genero = document.getElementById("genero").value;
-    let obraSocial = document.getElementById("obraSocial").value;
-    let confPass = document.getElementById("confPass").value;
+  if (contraseña != confPass) {
+    return alert("Las contraseñas no coinciden");
+  }
+  const validarEmail = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/;
+  const resultadoValidacion = validarEmail.test(email);
 
-    // if (contraseña = "") {
-    //     return alert("La contraseña no puede ser vacia ")
-    // }
-    
-
-    if (contraseña != confPass) {
-        return alert("Las contraseñas no coinciden");
-    }
-    const validarEmail = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/;
-	const resultadoValidacion = validarEmail.test(email);
-
-	if (!resultadoValidacion) {
-		console.log('no es un email valido');
-	} 
-
-    let comprobandoExistenciaEmail = usuariosPaciente.find((paciente) => {
-        return paciente.email === email;
+  if (!resultadoValidacion) {
+    return Swal.fire({
+      icon: "error",
+      title: "Oops...",
+      text: "No es un email válido",
     });
+  }
 
-    if (comprobandoExistenciaEmail) {
-        return alert("Ya esta registrado un usuario con ese email")
-    }
+  let comprobandoExistenciaEmail = usuariosPaciente.find((paciente) => {
+    return paciente.email === email;
+  });
 
-    usuariosPaciente.push(
-        new Paciente(nombreCompleto, fechaNac, email, contraseña, genero, obraSocial, this.rol)
+  if (comprobandoExistenciaEmail) {
+    Swal.fire({
+      icon: "error",
+      title: "Oops...",
+      text: "El Correo ingresado ya esta registrado",
+    });
+    return;
+  }
+
+  usuariosPaciente.push(
+    new Paciente(
+      nombreCompleto,
+      fechaNac,
+      email,
+      contraseña,
+      genero,
+      obraSocial,
+      this.rol
     )
+  );
 
-    localStorage.setItem("usuariosPaciente", JSON.stringify(usuariosPaciente));
+  localStorage.setItem("usuariosPaciente", JSON.stringify(usuariosPaciente));
 
-     document.getElementById("formulario").reset();
+  document.getElementById("formulario").reset();
 
+  Swal.fire({
+    position: "center",
+    icon: "success",
+    title: "Usuario registrado con éxito",
+    showConfirmButton: false,
+    timer: 2000,
+  });
 
-     Swal.fire({
-        position: "center",
-        icon: "success",
-        title: "Usuario registrado con éxito",
-        showConfirmButton: false,
-        timer: 2000,
-      });
-
-
-}
-formulario.addEventListener("submit", handleSubmit)
+  setTimeout(() => {
+    location.replace("loginPaciente.html");
+  }, 2500);
+};
+formulario.addEventListener("submit", handleSubmit);
