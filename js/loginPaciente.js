ab5efe9f838a008d84e863304fa152b40cfd1e4f
Lourdes Billone <lou.billone@gmail.com>
2024-03-23 17:11:56 -0300

    login pacientes

---
+++ js/loginPaciente.js
@@ -0,0 +1,31 @@
+let usuariosPaciente =
+  JSON.parse(localStorage.getItem("usuariosPaciente")) || [];
+let formularioPaciente = document.getElementById("formularioPaciente");
+
+const handleSubmit = (e) => {
+  e.preventDefault();
+
+  inicioSesionPaciente();
+};
+
+const inicioSesionPaciente = () => {
+  let email = document.getElementById("emailPaciente").value;
+  let password = document.getElementById("passPaciente").value;
+
+  let validarPaciente = usuariosPaciente.find((usuario) => {
+    return usuario.email === email;
+  });
+
+  if (validarPaciente) {
+    if (validarPaciente.contraseña === password) {
+      localStorage.setItem("loginAdmin", JSON.stringify(validarPaciente));
+      location.replace("/index.html");
+    } else {
+      alert("Su correo o contraseña es incorrecto, verifique sus credenciales");
+    }
+  } else {
+    alert("Su correo o contraseña es incorrecto, verifique sus credenciales");
+  }
+};
+
+formularioPaciente.addEventListener("submit", handleSubmit);
