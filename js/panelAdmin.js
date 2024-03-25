cda7265c69164f7524eac9870895794b56a47a7a
Lourdes Billone <lou.billone@gmail.com>
2024-03-25 11:50:24 -0300

    Agrego mensaje de bienvenida en el panel admin

--- js/panelAdmin.js
+++ js/panelAdmin.js
@@ -1,6 +1,18 @@
 let usuarioLogueado = JSON.parse(localStorage.getItem("login")) || null;
 let contenedorLista = document.getElementById("menu-lista");
 let formMenu = document.getElementById("formulario-menu");
+let containerMensaje = document.getElementById("mensajeBienvenida");
+
+/*FUNCIÓN MENSAJE DE BIENVENIDA */
+const mensajebienvenida = () => {
+  let h1 = document.createElement("h1");
+  h1.classList = `my-3`;
+  let cuerpoMensaje = `Bienvenido ${usuarioLogueado.nombre}`;
+  h1.innerText = cuerpoMensaje;
+  containerMensaje.appendChild(h1);
+};
+
+mensajebienvenida();
 
 //CERRAR SESIÓN
 if (usuarioLogueado) {
